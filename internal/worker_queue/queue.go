d81fcbc0abc979d071ac4b17651ad056f64431fe
kangho <kangho@hits.ai>
2024-08-24 15:28:44 +0900

    feat: 간단한 임시 retry 로직 추가

--- internal/worker_queue/queue.go
+++ internal/worker_queue/queue.go
@@ -3,6 +3,7 @@ package worker_queue
 import (
 	"github.com/kvngho/vimeovideoconverter/internal/application"
 	"github.com/rs/zerolog/log"
+	"time"
 )
 
 type JobDescription struct {
@@ -10,17 +11,23 @@ type JobDescription struct {
 	videoType string
 }
 
+type JobFailDescription struct {
+	JobDescription
+	FailedCount int
+}
 type WorkerPool struct {
-	MaxWorkers int
-	JobQueue   chan JobDescription
-	JobAction  *application.VideoUpdateService
+	MaxWorkers     int
+	JobQueue       chan JobDescription
+	FailedJobQueue chan JobFailDescription
+	JobAction      *application.VideoUpdateService
 }
 
 func NewWorkerPool(maxWorkers int, videoSvc *application.VideoUpdateService) *WorkerPool {
 	pool := &WorkerPool{
-		MaxWorkers: maxWorkers,
-		JobQueue:   make(chan JobDescription, 100),
-		JobAction:  videoSvc,
+		MaxWorkers:     maxWorkers,
+		JobQueue:       make(chan JobDescription, 100),
+		FailedJobQueue: make(chan JobFailDescription, 100),
+		JobAction:      videoSvc,
 	}
 	return pool
 }
@@ -33,17 +40,39 @@ func (wp *WorkerPool) Start() {
 				log.Info().Str("video_id", job.videoURL).Str("video_type", job.videoType).Msg("Job started")
 				convertedUrl, err := wp.JobAction.UpdateInformation(job.videoURL, job.videoType)
 				if err != nil {
-					log.Error().Err(err).Str("video_id", job.videoURL).Msg("Job failed")
+					log.Info().Err(err).Str("video_id", job.videoURL).Msg("Job failed - Submit the job to failed queue")
+					wp.ReSubmitJob(job, 1)
 				} else {
 					log.Info().Str("video_id", job.videoURL).Str("original_url", job.videoURL).Str("converted_url", convertedUrl).Msg("Job ended")
 				}
 			}
 		}(index)
 	}
+	// Handler Failed Job
+	go func() {
+		log.Info().Msg("Start handling failed job")
+		for failedJob := range wp.FailedJobQueue {
+			if failedJob.FailedCount > 3 {
+				log.Error().Str("video_url", failedJob.videoURL).Msg("Error. This video url is not yet valid")
+			}
+			time.Sleep(30 * time.Second) // 모든 작업에 대해서 5초 대기, 추후 수정 필요
+			convertedUrl, err := wp.JobAction.UpdateInformation(failedJob.videoURL, failedJob.videoType)
+			if err != nil {
+				log.Info().Str("video_id", failedJob.videoURL).Int("retry", failedJob.FailedCount).Msg("Job failed - Requeue")
+				wp.ReSubmitJob(failedJob.JobDescription, failedJob.FailedCount+1)
+			} else {
+				log.Info().Str("video_id", failedJob.videoURL).Str("original_url", failedJob.videoURL).Str("converted_url", convertedUrl).Msg("Job ended")
+			}
+		}
+	}()
 }
 
 func (wp *WorkerPool) SubmitJob(videoURL string, videoType string) {
 	wp.JobQueue <- JobDescription{videoURL: videoURL, videoType: videoType}
+}
+
+func (wp *WorkerPool) ReSubmitJob(jobDesc JobDescription, failedCount int) {
+	wp.FailedJobQueue <- JobFailDescription{jobDesc, failedCount}
 }
 
 func (wp *WorkerPool) Stop() {
